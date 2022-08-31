45a8976bb5ddafad4d3edc5bccd912716d5beffe
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2022-08-31 19:08:31 +0200

    [CPU] Added core emulation

---
+++ generators_new_gapy/ips/iss/core_emulation.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2022 ETH Zurich and University of Bologna
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import gsystree as st
+import os.path
+
+class CoreEmulation(st.Component):
+    def __init__(self,
+            parent,
+            name,
+            cluster_id: int=0,
+            core_id: int=0,
+            fetch_enable: bool=False,
+            *kargs, **kwargs):
+
+        super().__init__(parent, name)
+
+        self.set_component('cpu.emulation.emulation')
+
+        self.add_property('fetch_enable', fetch_enable)
+
---
+++ models/cpu/emulation/emulation.cpp
@@ -0,0 +1,341 @@
+/*
+ * Copyright (C) 2020 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Authors: Germain Haugou, ETH Zurich (germain.haugou@iis.ee.ethz.ch)
+ */
+
+#include <vp/vp.hpp>
+#include <vp/itf/io.hpp>
+#include <vector>
+#include <thread>
+#include <unistd.h>
+#include <mutex>
+#include <condition_variable>
+#include <gv/gvsoc.hpp>
+
+static int nb_running = 0;
+
+class emulation : public vp::component, public gv::Io_binding
+{
+
+public:
+    emulation(js::config *config);
+
+    int build();
+    void start();
+    void reset(bool active);
+    void stop();
+
+    void *external_bind(std::string comp_name, std::string itf_name, void *handle);
+
+    void grant(gv::Io_request *req);
+    void reply(gv::Io_request *req);
+    void access(gv::Io_request *req);
+
+private:
+    void sync_state(std::unique_lock<std::mutex> &lock);
+    void check_state();
+    static void clock_sync(void *__this, bool active);
+    static void fetchen_sync(void *__this, bool active);
+    static void bootaddr_sync(void *__this, uint32_t value);
+    static void irq_req_sync(void *__this, int irq);
+    static void data_grant(void *__this, vp::io_req *req);
+    static void data_response(void *__this, vp::io_req *req);
+
+    vp::trace trace;
+
+    vp::io_slave dbg_unit;
+    vp::wire_slave<uint32_t> bootaddr_itf;
+    vp::io_master data;
+    vp::io_master fetch;
+    vp::wire_slave<int>      irq_req_itf;
+    vp::wire_master<int>     irq_ack_itf;
+    vp::wire_slave<bool>     flush_cache_itf;
+    vp::wire_slave<bool>     flush_cache_ack_itf;
+    vp::wire_master<bool>    flush_cache_req_itf;
+    vp::wire_slave<bool>     halt_itf;
+    vp::wire_master<bool>    halt_status_itf;
+    vp::wire_master<bool>     busy_itf;
+    vp::wire_master<uint32_t> ext_counter[32];
+    vp::wire_slave<bool>     clock_itf;
+    vp::wire_slave<bool>     fetchen_itf;
+
+    std::vector<std::thread> threads;
+
+    gv::Io_user   *user;
+
+    std::mutex mutex;
+    std::condition_variable cond;
+
+    vp::io_req core_req;
+
+    bool reset_value;
+    bool fetchen_value;
+    bool clock_value;
+    bool is_active;
+    bool stalled;
+};
+
+
+static __thread void *__this;
+
+
+emulation::emulation(js::config *config)
+    : vp::component(config)
+{
+}
+
+void emulation::reset(bool active)
+{
+    this->trace.msg(vp::trace::LEVEL_DEBUG, "Setting reset (active: 0x%x)\n", active);
+    if (active)
+    {
+    }
+
+    this->reset_value = active;
+    std::unique_lock<std::mutex> lock(this->mutex);
+    this->check_state();
+    lock.unlock();
+}
+
+void *emulation::external_bind(std::string comp_name, std::string itf_name, void *handle)
+{
+    if (comp_name == this->get_path())
+    {
+        this->user = (gv::Io_user *)handle;
+        return static_cast<gv::Io_binding *>(this);
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+
+void emulation::grant(gv::Io_request *io_req)
+{
+    printf("%s %d\n", __FILE__, __LINE__);
+}
+
+void emulation::reply(gv::Io_request *io_req)
+{
+    printf("%s %d\n", __FILE__, __LINE__);
+}
+
+void emulation::access(gv::Io_request *req)
+{
+    this->trace.msg(vp::trace::LEVEL_DEBUG,
+        "Received IO req  (offset: 0x%llx, size: 0x%x, type: %d)\n",
+        req->addr, req->size, req->type);
+
+    if (req->type == 2)
+    {
+        std::unique_lock<std::mutex> lock(this->mutex);
+        this->sync_state(lock);
+        lock.unlock();
+    }
+    else
+    {
+        this->core_req.init();
+        this->core_req.set_addr(req->addr);
+        this->core_req.set_size(req->size);
+        this->core_req.set_is_write(req->type == gv::Io_request_write);
+        this->core_req.set_data(req->data);
+
+        this->get_engine()->lock();
+        int err = this->data.req(&this->core_req);
+        this->get_engine()->unlock();
+
+        if (err == vp::IO_REQ_OK || err == vp::IO_REQ_INVALID)
+        {
+            req->retval = err == vp::IO_REQ_INVALID ? gv::Io_request_ko : gv::Io_request_ok;
+            this->user->reply(req);
+        }
+        else
+        {
+            std::unique_lock<std::mutex> lock(this->mutex);
+            this->stalled = true;
+            this->check_state();
+            this->sync_state(lock);
+            lock.unlock();
+        }
+    }
+}
+
+void emulation::sync_state(std::unique_lock<std::mutex> &lock)
+{
+    if (!this->is_active)
+    {
+        nb_running--;
+        if (nb_running == 0)
+        {
+            this->trace.msg(vp::trace::LEVEL_DEBUG, "Running engine\n");
+            this->get_engine()->run();
+        }
+
+        while(!this->is_active)
+        {
+            this->cond.wait(lock);
+        }
+
+        nb_running++;
+        if (nb_running == 1)
+        {
+            this->trace.msg(vp::trace::LEVEL_DEBUG, "Stopping engine\n");
+            this->get_engine()->pause();
+        }
+    }
+}
+
+void emulation::check_state()
+{
+    this->trace.msg(vp::trace::LEVEL_DEBUG,
+        "Checking core state (is_active: %d, reset: %d, fetchen: %d, clock: %d, stalled: %d)\n",
+        this->is_active, this->reset_value, this->fetchen_value, this->clock_value, this->stalled
+    );
+
+    if (this->is_active)
+    {
+        if (this->reset_value || !this->fetchen_value || !this->clock_value || this->stalled)
+        {
+            this->trace.msg(vp::trace::LEVEL_DEBUG, "Deactivating core\n");
+            this->is_active = false;
+        }
+    }
+    else
+    {
+        if (!this->reset_value && this->fetchen_value && this->clock_value && !this->stalled)
+        {
+            this->trace.msg(vp::trace::LEVEL_DEBUG, "Activating core\n");
+            this->is_active = true;
+            this->cond.notify_all();
+        }
+    }
+}
+
+void emulation::data_grant(void *__this, vp::io_req *req)
+{
+}
+
+void emulation::data_response(void *__this, vp::io_req *req)
+{
+    emulation *_this = (emulation *)__this;
+    std::unique_lock<std::mutex> lock(_this->mutex);
+    _this->stalled = false;
+    _this->check_state();
+    lock.unlock();
+}
+
+
+void emulation::stop()
+{
+}
+
+
+int emulation::build()
+{
+    traces.new_trace("trace", &trace, vp::DEBUG);
+
+    new_slave_port("dbg_unit", &this->dbg_unit);
+
+    bootaddr_itf.set_sync_meth(&emulation::bootaddr_sync);
+    new_slave_port("bootaddr", &this->bootaddr_itf);
+
+    this->data.set_resp_meth(&emulation::data_response);
+    this->data.set_grant_meth(&emulation::data_grant);
+    new_master_port("data", &this->data);
+    new_master_port("fetch", &this->fetch);
+
+    this->irq_req_itf.set_sync_meth(&emulation::irq_req_sync);
+    new_slave_port("irq_req", &irq_req_itf);
+    new_master_port("irq_ack", &this->irq_ack_itf);
+
+    new_slave_port("flush_cache", &this->flush_cache_itf);
+    new_slave_port("flush_cache_ack", &this->flush_cache_ack_itf);
+    new_master_port("flush_cache_req", &this->flush_cache_req_itf);
+
+    new_slave_port("halt", &this->halt_itf);
+    new_master_port("halt_status", &this->halt_status_itf);
+
+    new_master_port("busy", &this->busy_itf);
+
+    this->clock_itf.set_sync_meth(&emulation::clock_sync);
+    new_slave_port("clock", &this->clock_itf);
+
+    this->fetchen_itf.set_sync_meth(&emulation::fetchen_sync);
+    new_slave_port("fetchen", &this->fetchen_itf);
+
+    for (int i=0; i<32; i++)
+    {
+        new_master_port("ext_counter[" + std::to_string(i) + "]", &this->ext_counter[i]);
+    }
+
+    nb_running++;
+    this->reset_value = true;
+    this->is_active = true;
+    this->clock_value = true;
+    this->stalled = false;
+    this->fetchen_value = get_js_config()->get("fetch_enable")->get_bool();
+
+    return 0;
+}
+
+
+void emulation::bootaddr_sync(void *__this, uint32_t value)
+{
+  emulation *_this = (emulation *)__this;
+  _this->trace.msg(vp::trace::LEVEL_DEBUG, "Setting boot address (value: 0x%x)\n", value);
+}
+
+
+void emulation::irq_req_sync(void *__this, int irq)
+{
+    emulation *_this = (emulation *)__this;
+    _this->trace.msg(vp::trace::LEVEL_DEBUG, "IRQ request (irq: %d) %d)\n", irq);
+}
+
+
+void emulation::fetchen_sync(void *__this, bool active)
+{
+    emulation *_this = (emulation *)__this;
+    _this->trace.msg(vp::trace::LEVEL_DEBUG,"Setting fetch enable (active: %d)\n", active);
+    _this->fetchen_value = active;
+    std::unique_lock<std::mutex> lock(_this->mutex);
+    _this->check_state();
+    lock.unlock();
+}
+
+
+void emulation::clock_sync(void *__this, bool active)
+{
+    emulation *_this = (emulation *)__this;
+    _this->trace.msg(vp::trace::LEVEL_DEBUG, "Setting clock (active: %d)\n", active);
+    _this->clock_value = active;
+    std::unique_lock<std::mutex> lock(_this->mutex);
+    _this->check_state();
+    lock.unlock();
+}
+
+
+void emulation::start()
+{
+}
+
+extern "C" vp::component *vp_constructor(js::config *config)
+{
+    return new emulation(config);
+}

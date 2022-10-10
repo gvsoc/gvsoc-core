4de79c857e121cea5cbb93d73d1d25fdacf68f61
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2022-10-10 13:17:00 +0200

    Switched license to Apache

--- generators/devices/ram/aps256xxn.py
+++ generators/devices/ram/aps256xxn.py
@@ -1,5 +1,4 @@
-# Copyright (C) 2022 GreenWaves Technologies, SAS, ETH Zurich and
-#                    University of Bologna
+# Copyright (C) 2020 GreenWaves Technologies, SAS, ETH Zurich and University of Bologna
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- models/interco/router_proxy.cpp
+++ models/interco/router_proxy.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 GreenWaves Technologies, SAS
+ * Copyright (C) 2021 GreenWaves Technologies, SAS, SAS, ETH Zurich and University of Bologna
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

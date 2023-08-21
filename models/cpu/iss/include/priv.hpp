2f6a85e7eb8de91d11b64c17ce63087f2a537c18
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2023-08-21 11:37:43 +0200

    Removed debug code

--- models/cpu/iss/include/priv.hpp
+++ models/cpu/iss/include/priv.hpp
@@ -76,34 +76,17 @@ static inline iss_reg_t csrrc_exec(Iss *iss, iss_insn_t *insn, iss_reg_t pc)
     return iss_insn_next(iss, insn, pc);
 }
 
-// static inline iss_insn_t *csrrs_exec(Iss *iss, iss_insn_t *insn)
-// {
-//     iss_reg_t value;
-//     iss_reg_t reg_value = REG_GET(0);
-
-//     if(UIM_GET(0) == 3104){
-//         if (insn->out_regs[0] != 0)
-//             REG_SET(0, iss->spatz.vl);
-//     }else if(UIM_GET(0) == 3105){
-//         if (insn->out_regs[0] != 0)
-//             REG_SET(0, iss->spatz.vtype);
-//     }else{
-//         if (iss_csr_read(iss, UIM_GET(0), &value) == 0)
-//         {
-//             if (insn->out_regs[0] != 0)
-//                 REG_SET(0, value);
-//         }
-//         if (REG_IN(0) != 0)
-//         {
-//             iss_csr_write(iss, UIM_GET(0), value | reg_value);
-//         }
-//     }
-//     return insn->next;
-// }
-static inline iss_insn_t *csrrs_exec(Iss *iss, iss_insn_t *insn)
+static inline iss_reg_t csrrs_exec(Iss *iss, iss_insn_t *insn, iss_reg_t pc)
 {
     iss_reg_t value;
     iss_reg_t reg_value = REG_GET(0);
+
+    CsrAbtractReg *csr = iss->csr.get_csr(UIM_GET(0));
+    if (csr && !csr->check_access(iss, REG_IN(0) != 0, true))
+    {
+        return pc;
+    }
+
     if (iss_csr_read(iss, UIM_GET(0), &value) == 0)
     {
         if (insn->out_regs[0] != 0)

2459fff68a2bb66a0d50ff56abe68814f991c994
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2024-05-16 09:47:40 +0200

    [OCCAMY] Fixed regressions

---
+++ models/cpu/iss/include/cores/snitch_bare/class.hpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2020 GreenWaves Technologies, SAS, ETH Zurich and
+ *                    University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * Authors: Germain Haugou, GreenWaves Technologies (germain.haugou@greenwaves-technologies.com)
+ */
+
+
+#pragma once
+
+
+#include <vp/vp.hpp>
+#include <vp/register.hpp>
+#include <cpu/iss/include/lsu.hpp>
+#include <cpu/iss/include/decode.hpp>
+#include <cpu/iss/include/trace.hpp>
+#include <cpu/iss/include/csr.hpp>
+#include <cpu/iss/include/dbgunit.hpp>
+#include <cpu/iss/include/exception.hpp>
+#include <cpu/iss/include/syscalls.hpp>
+#include <cpu/iss/include/timing.hpp>
+#include <cpu/iss/include/regfile.hpp>
+#ifdef CONFIG_GVSOC_ISS_RISCV_EXCEPTIONS
+#include <cpu/iss/include/irq/irq_riscv.hpp>
+#else
+#include <cpu/iss/include/irq/irq_external.hpp>
+#endif
+#include <cpu/iss/include/core.hpp>
+#include <cpu/iss/include/mmu.hpp>
+#include <cpu/iss/include/pmp.hpp>
+#include <cpu/iss/include/insn_cache.hpp>
+#include <cpu/iss/include/exec/exec_inorder.hpp>
+#include <cpu/iss/include/prefetch/prefetch_single_line.hpp>
+#include <cpu/iss/include/gdbserver.hpp>
+
+#if defined(CONFIG_GVSOC_ISS_INC_SPATZ)
+#include <cpu/iss/include/spatz.hpp>
+#endif
+
+class IssWrapper;
+
+
+class Iss
+{
+public:
+    Iss(IssWrapper &top);
+
+    Exec exec;
+    InsnCache insn_cache;
+    Timing timing;
+    Core core;
+    Regfile regfile;
+    Prefetcher prefetcher;
+    Decode decode;
+    Irq irq;
+    Gdbserver gdbserver;
+    Lsu lsu;
+    DbgUnit dbgunit;
+    Syscalls syscalls;
+    Trace trace;
+    Csr csr;
+    Mmu mmu;
+    Pmp pmp;
+    Exception exception;
+
+    vp::Component &top;
+
+#if defined(CONFIG_GVSOC_ISS_INC_SPATZ)
+    Spatz spatz;
+#endif
+
+
+
+private:
+    bool barrier_update(bool is_write, iss_reg_t &value);
+    static void barrier_sync(vp::Block *__this, bool value);
+
+    vp::WireMaster<bool> barrier_req_itf;
+    vp::WireSlave<bool> barrier_ack_itf;
+    CsrReg barrier;
+    bool waiting_barrier;
+};
+
+
+class IssWrapper : public vp::Component
+{
+
+public:
+    IssWrapper(vp::ComponentConf &config);
+
+    void start();
+    void reset(bool active);
+
+    Iss iss;
+
+private:
+    vp::Trace trace;
+};
+
+
+#include "cpu/iss/include/isa/rv64i.hpp"
+#include "cpu/iss/include/isa/rv32i.hpp"
+#if defined(CONFIG_GVSOC_ISS_INC_SPATZ)
+#include "cpu/iss/include/isa/rv32v.hpp"
+#endif
+#include "cpu/iss/include/isa/rv32c.hpp"
+#include "cpu/iss/include/isa/zcmp.hpp"
+#include "cpu/iss/include/isa/rv32a.hpp"
+
+#if ISS_REG_WIDTH == 64
+#include "cpu/iss/include/isa/rv64c.hpp"
+#endif
+#include "cpu/iss/include/isa/rv32m.hpp"
+#include "cpu/iss/include/isa/rv64m.hpp"
+#include "cpu/iss/include/isa/rv64a.hpp"
+#include "cpu/iss/include/isa/rvd.hpp"
+#include "cpu/iss/include/isa/rvf.hpp"
+#include "cpu/iss/include/isa/rvXf16.hpp"
+#include "cpu/iss/include/isa/rvXf16alt.hpp"
+#include "cpu/iss/include/isa/rvXf8.hpp"
+#include "cpu/iss/include/isa/rv32Xfvec.hpp"
+#include "cpu/iss/include/isa/rv32Xfaux.hpp"
+#include "cpu/iss/include/isa/priv.hpp"
+#include <cpu/iss/include/isa/xdma.hpp>
+
+#include <cpu/iss/include/exec/exec_inorder_implem.hpp>
---
+++ models/cpu/iss/src/snitch_bare/snitch.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2020 GreenWaves Technologies, SAS, ETH Zurich and
+ *                    University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Authors: Germain Haugou, GreenWaves Technologies (germain.haugou@greenwaves-technologies.com)
+ */
+
+#include "cpu/iss/include/iss.hpp"
+#include <string.h>
+
+
+Iss::Iss(IssWrapper &top)
+    : prefetcher(*this), exec(top, *this), insn_cache(*this), decode(*this), timing(*this), core(*this), irq(*this),
+      gdbserver(*this), lsu(*this), dbgunit(*this), syscalls(top, *this), trace(*this), csr(*this),
+      regfile(*this), mmu(*this), pmp(*this), exception(*this), top(top)
+#if defined(CONFIG_GVSOC_ISS_INC_SPATZ)
+      , spatz(*this)
+#endif
+{
+    this->csr.declare_csr(&this->barrier,  "barrier",   0x7C2);
+    this->barrier.register_callback(std::bind(&Iss::barrier_update, this, std::placeholders::_1,
+        std::placeholders::_2));
+
+    this->barrier_ack_itf.set_sync_meth(&Iss::barrier_sync);
+    this->top.new_slave_port("barrier_ack", &this->barrier_ack_itf, (vp::Block *)this);
+
+    this->top.new_master_port("barrier_req", &this->barrier_req_itf, (vp::Block *)this);
+}
+
+
+// This gets called when the barrier csr is accessed
+bool Iss::barrier_update(bool is_write, iss_reg_t &value)
+{
+    if (!is_write)
+    {
+        // Since syncing the barrier can immediatly trigger it, we need
+        // to flag that we are waiting fro the barrier.
+        this->waiting_barrier = true;
+
+        // Notify the global barrier
+        if (this->barrier_req_itf.is_bound())
+        {
+            this->barrier_req_itf.sync(1);
+        }
+
+        // Now we have to check if the barrier was already reached and the barrier
+        // sync function already called.
+        if (this->waiting_barrier)
+        {
+            // If not, stall the core, this will get unstalled when the barrier sync is called
+            this->exec.insn_stall();
+        }
+    }
+
+    return false;
+}
+
+
+// This gets called when the barrier is reached
+void Iss::barrier_sync(vp::Block *__this, bool value)
+{
+    Iss *_this = (Iss *)__this;
+
+    // Clear the flag
+    _this->waiting_barrier = false;
+
+    // In case the barrier is reached as soon as we notify it, we are not yet stalled
+    // and should not to anything
+    if (_this->exec.is_stalled())
+    {
+        // Otherwise unstall the core
+        _this->exec.stalled_dec();
+        _this->exec.insn_terminate();
+    }
+}

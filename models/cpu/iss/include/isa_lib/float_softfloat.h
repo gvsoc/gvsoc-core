9422ee8f0cf8e7265c9729d81080c80f3c4ee364
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2024-08-14 22:19:03 +0200

    softfloat: added sanitizer, for now only always returning same NAN value

--- models/cpu/iss/include/isa_lib/float_softfloat.h
+++ models/cpu/iss/include/isa_lib/float_softfloat.h
@@ -26,6 +26,21 @@
 #include "cpu/iss/include/isa_lib/macros.h"
 #include "cpu/iss/softfloat/softfloat.h"
 
+static int is_nan_32(uint32_t f) {
+    uint32_t exp = (f >> 23) & 0xFF;
+    uint32_t frac = f & 0x7FFFFF;
+    return (exp == 0xFF && frac != 0);
+}
+
+static uint32_t sanitize_32(uint32_t value)
+{
+    if (is_nan_32(value))
+    {
+      return 0x7FC00000;
+    }
+    return value;
+}
+
 static inline void float_set_rounding_mode(Iss *iss, int mode)
 {
     if (mode == 7)
@@ -38,35 +53,35 @@ static inline void float_set_rounding_mode(Iss *iss, int mode)
 static inline uint32_t float_add_32(Iss *iss, uint32_t a, uint32_t b, uint32_t mode)
 {
     float_set_rounding_mode(iss, mode);
-    return f32_add(iss, {.v=a}, {.v=b}).v;
+    return sanitize_32(f32_add(iss, {.v=a}, {.v=b}).v);
 }
 
 static inline uint32_t float_sub_32(Iss *iss, uint32_t a, uint32_t b, uint32_t mode)
 {
     float_set_rounding_mode(iss, mode);
-    return f32_sub(iss, {.v=a}, {.v=b}).v;
+    return sanitize_32(f32_sub(iss, {.v=a}, {.v=b}).v);
 }
 
 static inline uint32_t float_madd_32(Iss *iss, uint32_t a, uint32_t b, uint32_t c, uint32_t mode)
 {
     float_set_rounding_mode(iss, mode);
-    return f32_mulAdd(iss, {.v=a}, {.v=b}, {.v=c}).v;
+    return sanitize_32(f32_mulAdd(iss, {.v=a}, {.v=b}, {.v=c}).v);
 }
 
 static inline uint32_t float_msub_32(Iss *iss, uint32_t a, uint32_t b, uint32_t c, uint32_t mode)
 {
     float_set_rounding_mode(iss, mode);
-    return f32_mulSub(iss, {.v=a}, {.v=b}, {.v=c}).v;
+    return sanitize_32(f32_mulSub(iss, {.v=a}, {.v=b}, {.v=c}).v);
 }
 
 static inline uint32_t float_nmadd_32(Iss *iss, uint32_t a, uint32_t b, uint32_t c, uint32_t mode)
 {
     float_set_rounding_mode(iss, mode);
-    return f32_NmulAdd(iss, {.v=a}, {.v=b}, {.v=c}).v;
+    return sanitize_32(f32_NmulSub(iss, {.v=a}, {.v=b}, {.v=c}).v);
 }
 
 static inline uint32_t float_nmsub_32(Iss *iss, uint32_t a, uint32_t b, uint32_t c, uint32_t mode)
 {
     float_set_rounding_mode(iss, mode);
-    return f32_NmulSub(iss, {.v=a}, {.v=b}, {.v=c}).v;
+    return sanitize_32(f32_NmulAdd(iss, {.v=a}, {.v=b}, {.v=c}).v);
 }

6c90fe59ce8b414dbf5ffc7c5b705d0156650dcb
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2025-06-13 13:44:13 +0200

    iss: added new vcd trace for tracking multi-threading

--- models/cpu/iss/riscv.py
+++ models/cpu/iss/riscv.py
@@ -496,7 +496,7 @@ class RiscvCommon(st.Component):
         gvsoc.gui.Signal(self, active, name='label', path='label', groups=['core'], display=gvsoc.gui.DisplayStringBox())
         gvsoc.gui.Signal(self, active, name='active', path='busy', groups=['core'],
             display=gvsoc.gui.DisplayLogicBox('ACTIVE'))
-        gvsoc.gui.Signal(self, active, name='PC', path='pc', groups=['pc'],
+        pc_signal = gvsoc.gui.Signal(self, active, name='PC', path='pc', groups=['pc'],
             properties={'is_hotspot': True})
 
         gvsoc.gui.SignalGenFunctionFromBinary(self, active, from_signal='pc',
@@ -527,6 +527,8 @@ class RiscvCommon(st.Component):
         gvsoc.gui.Signal(self, stalls, name="st_ext_cycles", path="pcer_st_ext_cycles", display=gvsoc.gui.DisplayPulse(), groups=['stall'])
         gvsoc.gui.Signal(self, stalls, name="tcdm_cont",     path="pcer_tcdm_cont",     display=gvsoc.gui.DisplayPulse(), groups=['stall'])
         gvsoc.gui.Signal(self, stalls, name="misaligned",    path="pcer_misaligned",    display=gvsoc.gui.DisplayPulse(), groups=['stall'])
+
+        thread = gvsoc.gui.SignalGenThreads(self, active, 'thread', 'pc', 'function')
 
         return active
 

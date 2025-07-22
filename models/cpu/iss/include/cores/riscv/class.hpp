35a55ce1e38b2d9bfb338e2e70978ebd571cf843
TheSSDGuy <lorzuo@gmail.com>
2025-07-22 16:02:11 +0200

    Added a working skeleton of the iDMA controller to the tile. New ISA, IRQ mgmt, fixed xif decoder

--- models/cpu/iss/include/cores/riscv/class.hpp
+++ models/cpu/iss/include/cores/riscv/class.hpp
@@ -114,7 +114,7 @@ inline Iss::Iss(IssWrapper &top)
 #include "cpu/iss/include/isa/rvd.hpp"
 #include "cpu/iss/include/isa/priv.hpp"
 
-#include <cpu/iss/include/isa/xdma.hpp> //ZL-MOD 15062025 add ifdef plz!!! kind of #ifdef MAGIA_CORE
+#include <cpu/iss/include/isa/magia_idma.hpp> //ZL-MOD 15062025 add ifdef plz!!! kind of #ifdef MAGIA_CORE
 #include <cpu/iss/include/isa/redmule.hpp> //ZL-MOD 15062025 add ifdef plz!!! kind of #ifdef MAGIA_CORE
 #include <cpu/iss/include/isa/fractal_sync.hpp> //ZL-MOD 27062025 add ifdef plz!!! kind of #ifdef MAGIA_CORE
 

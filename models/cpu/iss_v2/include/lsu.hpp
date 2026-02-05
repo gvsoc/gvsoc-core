2cf068e08500fa81b6e9f09e39a1c92ec2cf77f1
Germain Haugou <germain.haugou@gmail.com>
2026-02-05 13:18:51 +0100

    Few fixes

--- models/cpu/iss_v2/include/lsu.hpp
+++ models/cpu/iss_v2/include/lsu.hpp
@@ -41,7 +41,7 @@ class Lsu
 public:
     Lsu(Iss &iss);
 
-    void start();
+    void start() {}
     void stop() {}
     void reset(bool active);
 
@@ -51,9 +51,6 @@ public:
     bool data_req_virtual(iss_insn_t *insn, iss_addr_t addr, int size, bool is_write, bool is_signed, int reg);
     bool data_req(iss_insn_t *insn, iss_addr_t addr, int size, bool is_write, bool is_signed, int reg);
     bool data_req_aligned(iss_insn_t *insn, iss_addr_t addr, int size, bool is_write, bool is_signed, int reg);
-
-    static void data_grant(vp::Block *__this, vp::IoReq *req);
-    static void data_response(vp::Block *__this, vp::IoReq *req);
 
     template<typename T>
     inline bool store(iss_insn_t *insn, iss_addr_t addr, int size, int reg);
@@ -87,64 +84,33 @@ public:
 
     bool atomic(iss_insn_t *insn, iss_addr_t addr, int size, int reg_in, int reg_out, vp::IoReqOpcode opcode);
 
-    inline void elw(iss_insn_t *insn, iss_addr_t addr, int size, int reg);
-    inline void elw_perf(iss_insn_t *insn, iss_addr_t addr, int size, int reg);
-
     bool lsu_is_empty();
 
-    // inline void stack_access_check(int reg, iss_addr_t addr);
+    // TODO used by syscalls, find a better to handle such accesses
+    vp::IoReq debug_req;
+    vp::IoMaster data;
 
+private:
     // Allocate a request. This can returns NULL if the core can not send more requests (last one
     // was denied or all requests are already allocated). In this case the core should stall.
     inline LsuReqEntry *get_req_entry();
     // Free a request so that it can be used for another access.
     // The cyclestamp indicates when the request becomes free. This allows easily freeing requests
     // during synchronous responses.
-    inline void free_req_entry(LsuReqEntry *entry, int64_t cyclestamp);
+    inline void free_req_entry(LsuReqEntry *entry);
+    bool load_req(iss_insn_t *insn, iss_addr_t addr, int size, int reg, bool is_signed);
+    static void handle_io_req_end(Iss *iss, Task *task);
+    static void data_grant(vp::Block *__this, vp::IoReq *req);
+    static void data_response(vp::Block *__this, vp::IoReq *req);
+
 
     Iss &iss;
 
     vp::Trace trace;
 
-
-    // lsu
-    vp::IoMaster data;
-    vp::WireMaster<void *> meminfo;
     LsuReqEntry *req_entry_first;
     LsuReqEntry req_entry[CONFIG_GVSOC_ISS_LSU_NB_OUTSTANDING];
-    vp::IoReq debug_req;
 
-    vp::reg_1 elw_stalled;
-
-    // A callback can be set here, so that it is called when the response of a pending
-    // request is received.
-#ifdef CONFIG_GVSOC_ISS_LSU_NB_OUTSTANDING
-    // When using multiple outstanding request, each on-going request can have its callback
-    // used when the response arrives.
-    void (*stall_callback[CONFIG_GVSOC_ISS_LSU_NB_OUTSTANDING])(Lsu *lsu, vp::IoReq *req);
-    int stall_reg[CONFIG_GVSOC_ISS_LSU_NB_OUTSTANDING];
-    int stall_size[CONFIG_GVSOC_ISS_LSU_NB_OUTSTANDING];
-#else
-    void (*stall_callback)(Lsu *lsu, vp::IoReq *req);
-    int stall_reg;
-    int stall_size;
-#endif
-    uint8_t *mem_array;
-    iss_addr_t memory_start;
-    iss_addr_t memory_end;
-
-private:
-    bool load_req(iss_insn_t *insn, iss_addr_t addr, int size, int reg, bool is_signed);
-
-    static void store_resume(void *_this, vp::IoReq *req);
-    static void store_resume(Lsu *lsu, vp::IoReq *req);
-    static void load_resume(Lsu *lsu, vp::IoReq *req);
-    static void elw_resume(Lsu *lsu, vp::IoReq *req);
-    static void load_signed_resume(Lsu *lsu, vp::IoReq *req);
-    static void load_float_resume(Lsu *lsu, vp::IoReq *req);
-    static void handle_io_req_end(Iss *iss, Task *task);
-
-    int64_t pending_latency;
     // True if the last request has been denied. The core must not send another request until
     // the last request has been granted
     vp::Signal<bool> io_req_denied;
